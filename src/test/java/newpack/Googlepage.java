d66f96de0be2e5153676a79881222c6388e43c30
Navhammer123 <snaveenkumar97@gmail.com>
2024-04-18 07:41:02 +0000

    new update

--- src/test/java/newpack/Googlepage.java
+++ src/test/java/newpack/Googlepage.java
@@ -1,15 +1,28 @@
 package newpack;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Googlepage {
 
 	public static void main(String[] args) {
+		
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.google.com/");
 		System.out.println("page title .." + driver.getTitle());
+		
+		WebElement search = driver.findElement(By.id("APjFqb"));
+		
+		search.sendKeys("cricket");
+		search.sendKeys(Keys.ENTER);
+		
+		
+		
 		
 
 	}
