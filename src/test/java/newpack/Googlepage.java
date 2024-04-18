fe014165e5a778f7462fc16e8a1981a69016e707
Navhammer123 <snaveenkumar97@gmail.com>
2024-04-18 05:37:08 +0000

    Initial project

---
+++ src/test/java/newpack/Googlepage.java
@@ -0,0 +1,16 @@
+package newpack;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Googlepage {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		
+
+	}
+
+}
