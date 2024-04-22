b8000190a3b253858b1e4aae57c987af9a662ea9
Navhammer123 <snaveenkumar97@gmail.com>
2024-04-22 08:15:24 +0000

    updated

--- src/test/java/newpack/check.java
+++ src/test/java/newpack/check.java
@@ -3,8 +3,6 @@
  */
 package newpack;
 
-
-
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -12,15 +10,22 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+/**
+ * @author Administrator
+ *
+ */
 public class check {
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://demo.seleniumeasy.com/table-sort-search-demo.html");
-		List<WebElement> N = driver.findElements(By.xpath("//td[text()='B. Wagner']//following-sibling::td"));
-	    System.out.println("iteams" + N.size());
-	    
-	    for(WebElement cell:N) {
+		List<WebElement> N = driver.findElements(By.xpath("//td[text()='New York']//preceding-sibling::td[2]"));
+		for (WebElement cell:N) {
+			System.out.println( cell.getText());
+		}
+				
 	}
-
 }
+	
+
+
